e21f825ec5fef7314f71bbd3226191f165d18802
Carly Ho <carlymho@gmail.com>
2018-11-25 02:33:37 +0000

    try and debug responses

--- activitypub.php
+++ activitypub.php
@@ -52,7 +52,7 @@
 	
 	function rewrite_init() {
 		// set up some nicer rewrite urls
-		add_rewrite_rule('^u/@([a-zA-Z0-9\-]+)/act/?$', 'index.php?rest_route=/ap/v1/actor&acct=$matches[1]', 'top');
+		add_rewrite_rule('^u/@([a-zA-Z0-9\-]+)/followers/?$', 'index.php?rest_route/ap/v1/followers&acct=$matches[1]', 'top');
 		add_rewrite_rule('^inbox/?$', 'index.php?rest_route=/ap/v1/inbox', 'top');
 		add_rewrite_rule('^u/@([a-zA-Z0-9\-]+)$', 'index.php?author_name=$matches[1]', 'top');
 		add_rewrite_rule('^\.well-known/([a-zA-Z0-9\-\?\=\@\%\.]+)$', 'index.php?rest_route=/ap/v1/$matches[1]', 'top');
@@ -66,6 +66,18 @@
 			'public' => true,
 			'supports' => array('title')
 		));
+		
+		register_post_type('inboxitem', array(
+			'label' => 'Inbox',
+			'public' => true,
+			'supports' => array('title', 'editor', 'page-attributes')
+		));
+		
+		register_post_type('outboxitem', array(
+			'label' => 'Outbox',
+			'public' => true,
+			'supports' => array('title', 'editor', 'page-attributes')
+		));
 	}
 	add_action('init', 'post_types_init');
 	
@@ -73,8 +85,8 @@
 		// get the url that was requested
 		$req = $_SERVER['REQUEST_URI'];
 		// parse it and see if it's an author url by our schema
-		preg_match('/\/u\/@([a-zA-Z0-9\-]+)\/?/$', $req, $matches1);
-		preg_match('/\/author\/([a-zA-Z0-9\-]+)\/?/$', $req, $matches2);
+		preg_match('/\/u\/@([a-zA-Z0-9\-]+)\/?$/', $req, $matches1);
+		preg_match('/\/author\/([a-zA-Z0-9\-]+)\/?$/', $req, $matches2);
 		if ((count($matches1) > 0 || count($matches2) > 0) && in_array('application/activity+json', explode(",", getallheaders()['Accept']))) {
 			// if it's an author and the request asked for application/activity+json format, return the actor
 			header('Content-type: application/activity+json');
@@ -88,7 +100,7 @@
 			// remove all the newlines characters from the public key and replace with \n for json
 			$safe_key = preg_replace('/\n/', '\n', trim(get_user_meta($user->ID, 'pubkey', true)));
 			// echo the composited actor json object
-			echo '{"@context": ["https://www.w3.org/ns/activitystreams", "https://w3id.org/security/v1"], "id": "'.get_bloginfo('url').'/u/@'.$user->user_login.'", "type": "Person", "preferredUsername": "'.$user->user_login.'", "inbox": "'.get_bloginfo('url').'/inbox", "icon": {"type": "Image", "mediaType": "image/jpeg", "url": "'.get_avatar_url($user->ID).'"}, "summary": "'.get_user_meta($user->ID, 'description').'", "publicKey": { "id": "'.get_bloginfo('url').'/u/@'.$user->user_login.'#main-key", "owner": "'.get_bloginfo('url').'/u/@'.$user->user_login.'", "publicKeyPem": "'.$safe_key.'"}}';
+			echo '{"@context": ["https://www.w3.org/ns/activitystreams", "https://w3id.org/security/v1"], "id": "'.get_bloginfo('url').'/u/@'.$user->user_login.'", "type": "Person", "preferredUsername": "'.$user->user_login.'", "inbox": "'.get_bloginfo('url').'/inbox", "manuallyApprovesFollowers": false, "icon": {"type": "Image", "mediaType": "image/jpeg", "url": "'.get_avatar_url($user->ID).'"}, "summary": "'.get_user_meta($user->ID, 'description', true).'", "publicKey": { "id": "'.get_bloginfo('url').'/u/@'.$user->user_login.'#main-key", "owner": "'.get_bloginfo('url').'/u/@'.$user->user_login.'", "publicKeyPem": "'.$safe_key.'"}}';
 			// close request
 			die(1);
 		}
@@ -123,20 +135,26 @@
 		}
 		die(1);
 	}
-
-	function get_webfinger_actor() {
-		// returns the activitypub actor data
+	
+	function get_followers() {
+		$user = get_query_var('author');
+		$users = get_users(array(
+			'role__in' => array('subscriber'),
+			'meta_query' => array(
+				array(
+					'key' => 'domain',
+					'compare' => 'EXISTS'
+				)
+			)
+		));
+		$content = array(
+			'@context' => 'https://www.w3.org/ns/activitystreams',
+			'type' => 'OrderedCollection',
+			'totalItems' => count($users),
+			'id' => 'https://'.get_bloginfo('url')['host'].'/u/@'.$user->login_name
+		);
 		header('Content-type: application/activity+json');
-		$acct = $_GET['acct'];
-		if ($acct) {
-			$user = get_user_by('slug', $acct);
-			// remove all the newlines characters from the public key and replace with \n for json
-			$safe_key = preg_replace('/\n/', '\n', trim(get_user_meta($user->ID, 'pubkey', true)));
-			// echo the composited actor json object
-			echo '{"@context": ["https://www.w3.org/ns/activitystreams", "https://w3id.org/security/v1"], "id": "'.get_bloginfo('url').'/u/@'.$user->user_login.'", "type": "Person", "preferredUsername": "'.$user->user_login.'", "inbox": "'.get_bloginfo('url').'/inbox", "publicKey": { "id": "'.get_bloginfo('url').'/u/@'.$user->user_login.'#main-key", "owner": "'.get_bloginfo('url').'/u/@'.$user->user_login.'", "publicKeyPem": "'.$safe_key.'"}}';
-		}
-
-		die(1);
+		echo json_encode($content);
 	}
 	
 	function get_actor($url) {
@@ -165,6 +183,11 @@
 		
 		// get actor contents
 		$entityBody = json_decode(file_get_contents('php://input'));
+		wp_insert_post(array(
+			'post_content' => file_get_contents('php://input'),
+			'post_status' => 'publish',
+			'post_type' => 'inboxitem'
+		));
 		$type = $entityBody->type;
 		$a = $entityBody->actor;
 		if ($a) {
@@ -174,11 +197,15 @@
 			// cobble together the webfinger url from the preferred username and the host name
 			$username = $act->preferredUsername.'@'.parse_url($entityBody->id)['host'];
 			// get the username we're trying to follow on this site
-			$followobj = (string)$entityBody->object;
+			$followobj = $entityBody->object."";
+			echo $followobj;
 			$following = str_replace('https://'.parse_url($followobj)['host']."/u/@", "", $followobj);
+			$domain = parse_url($entityBody->id)['host'];
+			
 			if ($following) {
 				// check if there's an account by that name
 				header('Content-type: application/activity+json');
+				header("HTTP/1.1 200 OK");
 				
 				if ($type == 'Follow') {
 					// if it's a follow request, process it
@@ -186,37 +213,83 @@
 					$bd = get_posts(array(
 						'post_type' => 'domain_block',
 						'posts_per_page' => -1,
-						'post_name' => str_replace('.', '-', parse_url($entityBody->id)['host'])
+						'post_name' => str_replace('.', '-', $domain)
 					));
 					if (count($bd) > 0) {
+						$p = wp_insert_post(array(
+							'post_type' => 'outboxitem',
+							'post_status' => 'publish',
+							'post_content' => 'pending'
+						));
 						$reject = '{"@context": "https://www.w3.org/ns/activitystreams", "id": "'.get_bloginfo('url').'/u/@'.$following.'", "type": "Reject", "actor": "'.get_bloginfo('url').'/u/@'.$following.'", "object": "'.$act->id.'"}';
+						wp_update_post(array(
+							'ID' => $p,
+							'post_content' => $reject
+						));
+						$ch = curl_init();
+						$date = date('c');
+						$pkey = openssl_get_privatekey(get_user_meta($follow_user->ID, 'privkey', true));
+						$str = "(request-target): post /inbox\nhost: ".$domain."\ndate: ".$date;
+						openssl_sign($str, $signature, $pkey);
+						$sig_str = 'keyId="'.get_bloginfo('url').'/u/@'.$following.'",headers="(request-target) host date",signature="' .$signature. '"';
+						curl_setopt($ch, CURLOPT_URL, $inbox);
+						curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+					    'Signature: '.$sig_str,
+					    'Date: '.$date,
+					    'Host: '.$domain,
+					    'Content-type: application/ld+json; profile="https://www.w3.org/ns/activitystreams'
+						));
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+						curl_setopt($ch, CURLOPT_POST, 1);
+						curl_setopt($ch, CURLOPT_POSTFIELDS, 'body='.$reject);
+						$result = curl_exec($ch);
+						curl_close($ch);
 						echo $reject;
 						die(1);
 						return;
 					}
+					$follow_user = get_user_by('username', $following);
 					// otherwise, add user account and return accept notice
 					$user_check = get_user_by('username', $username);
 					if (!$user_check) {
 						$u = wp_create_user($username, serialize(bin2hex(random_bytes(16))));
 						// store inbox url, preferred username and domain for reference purposes
-						update_user_meta($u, 'description', json_encode($entityBody));
 						update_user_meta($u, 'inbox', $inbox);
 						update_user_meta($u, 'preferred_username', $act->preferredUsername);
-						update_user_meta($u, 'domain', parse_url($entityBody->id)['host']);
+						update_user_meta($u, 'description', json_encode($entityBody));
+						update_user_meta($u, 'domain', $domain);
 					}
 					// create acceptance object
-					$accept = '{"@context": "https://www.w3.org/ns/activitystreams", "id": "'.get_bloginfo('url').'/u/@'.$following.'", "type": "Accept", "actor": "'.get_bloginfo('url').'/u/@'.$following.'", "object": "'.$act->id.'"}';
+					$p = wp_insert_post(array(
+						'post_type' => 'outboxitem',
+						'post_content' => 'pending',
+						'post_status' => 'publish'
+					));
+					$accept = '{"@context": "https://www.w3.org/ns/activitystreams", "id": "'.get_the_permalink($p).'", "type": "Accept", "actor": "'.get_bloginfo('url').'/u/@'.$following.'", "object": "'.$act->id.'"}';
+					wp_update_post(array(
+						'ID' => $p,
+						'post_content' => $accept
+					));
 					
 					$ch = curl_init();
-					$fields = array(
-						'body' => $accept
-					);
+					$date = date('c');
+					$pkey = openssl_get_privatekey(get_user_meta($follow_user->ID, 'privkey', true));
+					$str = "(request-target): post /inbox\nhost: ".$domain."\ndate: ".$date;
+					openssl_sign($str, $signature, $pkey);
+					$sig_str = 'keyId="'.get_bloginfo('url').'/u/@'.$following.'",headers="(request-target) host date",signature="' .$signature. '"';
 					curl_setopt($ch, CURLOPT_URL, $inbox);
+					curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+				    'Signature: '.$sig_str,
+				    'Date: '.$date,
+				    'Host: '.$domain,
+				    'Content-type: application/ld+json; profile="https://www.w3.org/ns/activitystreams'
+					));
 					curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-					curl_setopt($ch, CURLOPT_POST, count($fields));
-					curl_setopt($ch, CURLOPT_POSTFIELDS, 'body='.$message);
+					curl_setopt($ch, CURLOPT_POST, 1);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, 'body='.$accept);
 					$result = curl_exec($ch);
 					curl_close($ch);
+					update_user_meta($u, 'description', json_encode($result));
 					echo $accept;
 				} elseif ($type == 'Unfollow') {
 					// if it's an unfollow request, process it
@@ -224,6 +297,8 @@
 					wp_delete_user($user->ID);
 					// delete user from database
 				}
+			} elseif ($type == 'Create') {
+				// handle replies here
 			} else {
 				echo "No user by that name.";
 			}
@@ -297,63 +372,75 @@
 	add_action( 'edit_user_profile', 'add_suspend_checkbox', 10, 1 );
 
 	function send_messages($post_id) {
-		// get all domain blocks and reduce down to an array of just titles (aka the actual domain)
-		$domain_blocks = array_map(get_posts(array(
-			'post_type' => 'domain_block',
-			'posts_per_page' => -1
-		)), function($x) {
-			return $x->post_title;
-		});
-		
-		// get a list of all subscribers who are not on a blocked domain and who have a valid activitypub webfinger username and who haven't been suspended
-		$subscribers = get_users(array(
-			'role' => 'subscriber',
-			'meta_query' => array(
-				'relation' => 'AND',
-				array(
+		global $post;
+		$post = get_post($post_id);
+		// only do the stuff if this is a regular blog post
+		if (get_post_type() == 'post') {
+			// get all domain blocks and reduce down to an array of just titles (aka the actual domain)
+			$domains = get_posts(array(
+				'post_type' => 'domain_block',
+				'posts_per_page' => -1
+			));
+			
+			if (count($domains) > 0) {
+				$domain_blocks = array_map($domains, function($x) {
+					return $x->post_title;
+				});
+			}
+			
+			// get a list of all subscribers who are not on a blocked domain and who have a valid activitypub webfinger username and who haven't been suspended
+			$subscribers = get_users(array(
+				'role' => 'subscriber',
+				'meta_query' => array(
+					'relation' => 'AND',
+					array(
+						'key' => 'ap_username',
+						'compare' => 'EXISTS'
+					),
+					array(
+						'key' => 'domain',
+						'compare' => 'EXISTS'
+					),
+					array(
+						'relation' => 'OR',
+						array(
+							'key' => 'suspended',
+							'value' => true,
+							'compare' => '!='
+						),
+						array(
+							'key' => 'suspended',
+							'compare' => 'NOT EXISTS'
+						)
+					)
+				)
+			));
+			
+			if (count($domains) > 0) {
+				array_push($subscribers['meta_query'], array(
 					'key' => 'domain',
 					'value' => $domain_blocks,
 					'compare' => 'NOT IN'
-				),
-				array(
-					'key' => 'ap_username',
-					'compare' => 'EXISTS'
-				),
-				array(
-					'key' => 'domain',
-					'compare' => 'EXISTS'
-				),
-				array(
-					'relation' => 'OR',
-					array(
-						'key' => 'suspended',
-						'value' => true,
-						'compare' => '!='
-					),
-					array(
-						'key' => 'suspended',
-						'compare' => 'NOT EXISTS'
-					)
-				)
-			)
-		));
-		
-		// replace newlines with \n because json doesn't like them
-		$filtered_content = preg_replace('/\n/', '\n', get_the_content());
-		// set up message object
-		$message = '{"@context": "https://www.w3.org/ns/activitystreams","id": "'.get_permalink($post_id).'", "type": "Create", "actor": "'.get_bloginfo('url').'/u/@'.get_the_author_meta('user_login').'", "object": {"id": "'.get_permalink($post_id).'", "type": "Note", "published": "'.get_the_date('c').'", "attributedTo": "'.get_bloginfo('url').'/u/@'.get_the_author_meta('user_login').'", "content": "'.filtered_content().'", "to": "https://www.w3.org/ns/activitystreams#Public"}}';
-		foreach ($subscribers as $subscriber) {
-			// post message to subscriber domains
-			$ch = curl_init();
-			$fields = array(
-				'body' => $message
-			);
-			curl_setopt($ch, CURLOPT_URL, get_user_meta($subscriber->ID, 'inbox', true));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-			curl_setopt($ch, CURLOPT_POST, count($fields));
-			curl_setopt($ch, CURLOPT_POSTFIELDS, 'body='.$message);
-			$result = curl_exec($ch);
-			curl_close($ch);
+				));
+			}
+			
+			// replace newlines with \n because json doesn't like them
+			$filtered_content = preg_replace('/\n/', '\n', get_the_content());
+			// set up message object
+			$message = '{"@context": "https://www.w3.org/ns/activitystreams","id": "'.get_permalink($post_id).'", "type": "Create", "actor": "'.get_bloginfo('url').'/u/@'.get_the_author_meta('user_login').'", "object": {"id": "'.get_permalink($post_id).'", "type": "Note", "published": "'.get_the_date('c').'", "attributedTo": "'.get_bloginfo('url').'/u/@'.get_the_author_meta('user_login').'", "content": "'.filtered_content().'", "to": "https://www.w3.org/ns/activitystreams#Public"}}';
+			foreach ($subscribers as $subscriber) {
+				// post message to subscriber domains
+				$ch = curl_init();
+				$fields = array(
+					'body' => $message
+				);
+				curl_setopt($ch, CURLOPT_URL, get_user_meta($subscriber->ID, 'inbox', true));
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+				curl_setopt($ch, CURLOPT_POST, count($fields));
+				curl_setopt($ch, CURLOPT_POSTFIELDS, 'body='.$message);
+				$result = curl_exec($ch);
+				curl_close($ch);
+			}
 		}
 		
 		return;
