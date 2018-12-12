57db611f0e09294deea716e79075bb8848e574c5
Carly Ho <yaysunshine@gmail.com>
2018-12-11 22:14:00 -0600

    try returning as single value

--- activitypub.php
+++ activitypub.php
@@ -449,7 +449,7 @@
 			print_r($user);
 			print_r($users);
 			$users = array_filter($users, function($x) {
-				return in_array($user->user_login, get_user_meta($user->ID, 'following'));
+				return in_array($user->user_login, get_user_meta($user->ID, 'following', true));
 			});
 			print_r($users);
 			die(1);
