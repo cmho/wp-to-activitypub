d6736ec56e534058ab79be6bf7492ae6812970a3
Carly Ho <yaysunshine@gmail.com>
2018-12-11 22:49:57 -0600

    adjust content-type parameter

--- activitypub.php
+++ activitypub.php
@@ -677,7 +677,7 @@ EOT;
 								'Signature: '.$sig_str,
 						    'Date: '.$date,
 						    'Host: '.$domain,
-								'Content-Type: application/ld+json; profile="https://www.w3.org/ns/activitystreams'
+								'Content-Type: application/activity+json'
 							),
 						));
 						$result = curl_exec($ch);
