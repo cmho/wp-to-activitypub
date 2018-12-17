2efccda11cf0d84eca91409e8263e591f062e586
Carly Ho <yaysunshine@gmail.com>
2018-12-17 00:59:07 -0600

    move to template redirect

--- activitypub.php
+++ activitypub.php
@@ -447,15 +447,15 @@
 	}
 	add_action('wp_headers', 'redirect_to_actor');
 
-	function redirect_to_status($h) {
-		if (in_array('application/activity+json', explode(",", getallheaders()['Accept']))) {
-					print_r($this);
-					header('Content-type: application/activity+json');
+	function redirect_to_status() {
+		global $post;
+		if (is_single() && get_post_type() == 'post' && in_array('application/activity+json', explode(",", getallheaders()['Accept']))) {
+			header('Content-type: application/activity+json');
 			echo get_post_meta($post->ID, 'object', true);
 			die(1);
 		}
 	}
-	add_action('wp_headers', 'redirect_to_status');
+	add_action('template_redirect', 'redirect_to_status');
 	
 	function add_head_links() {
 		if (is_author()) {
