70f32d82137a7b785399fc132f017b59d2d3b385
Carly Ho <yaysunshine@gmail.com>
2018-12-17 00:41:41 -0600

    move global  earlier

--- activitypub.php
+++ activitypub.php
@@ -448,8 +448,8 @@
 	add_action('wp_headers', 'redirect_to_actor');
 
 	function redirect_to_status() {
+		global $post;
 		if (is_single() && get_post_type() == 'post' && in_array('application/activity+json', explode(",", getallheaders()['Accept']))) {
-			global $post;
 			header('Content-type: application/activity+json');
 			echo get_post_meta($post->ID, 'object', true);
 			die(1);
